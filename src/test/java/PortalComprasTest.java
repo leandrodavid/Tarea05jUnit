2d5db1f82ef8fb179f7ceb619f3416637cdf98f1
Leandro <leandrodavidm@hotmail.com>
2021-11-26 23:33:15 -0300

    Agregado de etiquetas

--- src/test/java/PortalComprasTest.java
+++ src/test/java/PortalComprasTest.java
@@ -1,5 +1,7 @@
 import laboratoriojUnit.Carrito;
 import laboratoriojUnit.Producto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import  org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -14,22 +16,30 @@ public class PortalComprasTest {
 
 
     @Test
+    @Order(1)
+    @DisplayName("Test para verificar que se vacie el carrito")
     public void vaciarCarritoTest() {
         carrito.vaciarCarrito();
         assertEquals(true, carrito.verificarCarrito(), "El carrito no se encuentra vacio");
     }
 
     @Test
+    @Order(3)
+    @DisplayName("Test para verificar si el carrito  está vacio")
     public void carritoVacioFallaTest() {
         carrito.agregarProducto(new Producto("pera", 45));
         assertTrue(carrito.verificarCarrito(), "El carrito no se encuentra vacio");
     }
 
     @Test
+    @Order(2)
+    @DisplayName("Test para verificar si el carrito no está vacio")
     public void carritoVacioNoVacioTest() {
         carrito.agregarProducto(new Producto("pera", 45));
         carrito.agregarProducto(new Producto("manzana", 28));
         assertFalse(carrito.verificarCarrito(), "El carrito  se encuentra vacio");
     }
+
+
 
 }
